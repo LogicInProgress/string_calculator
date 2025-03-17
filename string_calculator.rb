d65bf4eacd43683454d8726c1e3f92b13888e43a
LogicInProgress <rahul9.98.be@gmail.com>
2025-03-17 16:49:20 +0000

    Add basic rspec tests for string calculator

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,24 +1,31 @@
 
-def add(string)
-  return 0 if string.empty?
-  raise RuntimeError if string.end_with?("/n")
+class StringCalculator
+  attr_reader :number_string
 
-  delimiter = ","
-
-  if string.start_with?("//")
-    string_with_delimiter = numbers.split("\n")
-    delimiter = string_with_delimiter.first[2]
-    string = string_with_delimiter.last
+  def initialize(string)
+    @number_string = string
   end
 
-  string = string.gsub("\n", delimiter)
-  numbers = string.split(delimiter).map(&:to_i)
-  
-  negative_numbers = numbers.select { |n| n < 0 }
-  if negative_numbers.any?
-    raise RuntimeError "negatives not allowed: #{negative_numbers.join(', ')}"
+  def add
+    return 0 if number_string.empty?
+    raise RuntimeError if number_string.end_with?("/n")
+
+    delimiter = ","
+
+    if number_string.start_with?("//")
+      string_with_delimiter = number_string.split("\n")
+      delimiter = string_with_delimiter.first[2]
+      string = string_with_delimiter.last
+    end
+
+    number_string.gsub!("\n", delimiter)
+    numbers = number_string.gsub("\n", delimiter).split(delimiter).map(&:to_i)
+    
+    negative_numbers = numbers.select { |n| n < 0 }
+    if negative_numbers.any?
+      raise "negatives not allowed: #{negative_numbers.join(', ')}"
+    end
+    
+    numbers.sum
   end
-  
-  numbers.sum
 end
-
